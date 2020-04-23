556b4bf4bc64399274c6ceba306e91ca27354c58
Khaldoon Mutahar <git@mutahar.me>
2020-04-23 13:29:27 +0300

    Default installer
    install ohmyzsh
    make new entertainment directory

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo
+echo "****************************"
+echo "*** set up ***"
+echo "****************************"
+echo
+
+
+## Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+mkdir ~/Entertainment
