05c28f4ce76cadb9739214b883d17c22a203cfd0
Khaldoon Mutahar <git@mutahar.me>
2020-04-23 13:05:48 +0300

    Symlinks

---
+++ symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -sf ~/.dotfiles/config ~/.config
+
+ln -sf ~/.dotfiles/aws ~/.aws
+
+ln -sf ~/.dotfiles/shell/zshrc ~/.zshrc
+
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+ln -sf ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+
+ln -sf ~/Entertainment/NzbDrone ~/.dotfiles/config/NzbDrone
+
+ln -sf ~/Entertainment/Radarr ~/.dotfiles/config/Radarr
