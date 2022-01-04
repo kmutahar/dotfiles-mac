3575c8849bacc2db9501265551d0fa268565aeb8
Khaldoon Mutahar <git@mutahar.me>
2022-01-04 22:00:11 +0300

    add node.sh installer script (Draft)

---
+++ node.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo
+echo "*********************"
+echo "*** NPM install ***"
+echo "*********************"
+echo
+
+
+npm install -g fast-cli
+npm install ghost-cli@latest -g
+npm install -g git-file-history
+npm install -g github-email
+npm install -g gscan
+npm install -g netflix-migrate
+npm install -g speed-test
+npm install -g conventional-changelog-cli
+npm install -g lighthouse
+
+
+#Other npm installed before
+
+# docsify-cli
+# eclint
+# git-pending
+# trash-cli
+# corona-cli
+# yarn
+# create-react-app
+# gatsby-cli
+# htmlhint
+# netlify
